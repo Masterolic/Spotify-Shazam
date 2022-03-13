956cf8ed14318f42665d211c2351541a4dec68ea
Masterolic <93469093+Masterolic@users.noreply.github.com>
2022-03-13 22:57:11 +0530

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -13,7 +13,7 @@ class bot(Client):
         config = ConfigParser()
         config.read(config_file)
         name = name.lower()
-        plugins = plugins 
+        plugins = {'root': path.join(__package__, 'plugins')} 
         api_id = "19566690"
         api_hash = "9f146b5dab7c76e7f8febec8e3df2501"
         super().__init__(
