f430c7a9081a0de4b6b1a2df788e2b7e5ba2a126
Masterolic <93469093+Masterolic@users.noreply.github.com>
2022-03-15 13:57:22 +0530

    Update shazam.py

--- bot/plugins/shazam.py
+++ bot/plugins/shazam.py
@@ -3,9 +3,9 @@ from pyrogram import filters
 
 
 @bot.on_message(
-    filters.command("start")
+    filters.command("features")
 )
 async def alive(_, message):
     await message.reply(
-       f"𝗙𝗘𝗔𝗧𝗨𝗥𝗘𝗦 /n__support Spotify  url__ /n__~A single song /n~Albums /n~Playlists /n~Artists__ /n𝗬𝗢𝗨𝗧𝗨𝗕𝗘 /n__Type song name__ /n𝗦𝗢𝗡𝗚 /n__Just send the music name__ /neg `Dil ko karar` /n𝗦𝗛𝗔𝗭𝗔𝗠 /n__Send your song here and wait ....!__ /n𝗚𝗥𝗢𝗨𝗣𝗦 /nYou can add me in groups 🥳🥳🥳 njoy 🥳🥳 more functions in group just type name....!"
+       f"𝗙𝗘𝗔𝗧𝗨𝗥𝗘𝗦 \n __support Spotify  url__ \n __~A single song \n ~Albums \n ~Playlists \n ~Artists__ \n\n 𝗬𝗢𝗨𝗧𝗨𝗕𝗘 \n __Type song name__ \n\n 𝗦𝗢𝗡𝗚 \n __Just send the music name__ \neg `Dil ko karar`\n\n 𝗦𝗛𝗔𝗭𝗔𝗠 \n __Send your song here and wait ....!__ \n\n 𝗚𝗥𝗢𝗨𝗣𝗦 \n You can add me in groups 🥳🥳🥳 njoy 🥳🥳 more functions in group just type name....!"
     )
